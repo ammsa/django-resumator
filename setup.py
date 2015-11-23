48777dcd64b97a56caef2388b2f4b2a004f0a555
Mustafa Abualsaud <ammsa7@gmail.com>
2015-11-22 20:18:28 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-resumator',
-    version='1.1.4',
+    version='1.1.5',
     packages=['resumator'],
     install_requires=[
         'django-solo>=1.1.0',
